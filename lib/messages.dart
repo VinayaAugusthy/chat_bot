187e2fb27eb7a20f031df2665763644a6c0f4bd2
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-12-13 18:28:03 +0530

    new query added

--- lib/messages.dart
+++ lib/messages.dart
@@ -6,7 +6,7 @@ final List<String> keywords = [
   'why i am not getting call from astrologer',
   'what is the minimum amount required to call an astrologer',
   'can i call the astrologer for free',
-  'why wallet is not updating after recharge'
+  'why wallet is not updating after recharge', 'thank you',
 ];
 
 final List<String> responses = [
@@ -16,5 +16,6 @@ final List<String> responses = [
   '1.Check your network connection.\n 2. Astrologer may be busy due to heavy responses...pls wait',
   'The minimum amount varies according to the rate per minute charged by each astrologer. You should have a minute of balance that lets you talk for 5 minutes.',
   'No, to call an astrologer, you have to recharge your wallet. You can chat with the astrologer for free.',
-  'It may due to technical reasons. If issue persist please contact our customer care.'
+  'It may due to technical reasons. If issue persist please contact our customer care.',
+  'You are welcome',
 ];
